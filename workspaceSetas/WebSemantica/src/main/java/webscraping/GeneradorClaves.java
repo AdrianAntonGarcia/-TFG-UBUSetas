59ca3dffcc85937de1e2a580d3f285d9f383b630
Adrian Anton Garcia <adrtler@gmail.com>
2017-11-02 18:47:01 +0100

    Introducción de comentarios al generador de claves.

--- workspaceSetas/WebSemantica/src/main/java/webscraping/GeneradorClaves.java
+++ workspaceSetas/WebSemantica/src/main/java/webscraping/GeneradorClaves.java
@@ -12,6 +12,13 @@ import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 
+/**
+ * @name GeneradorClaves
+ * @author Adrian Anton Garcia
+ * @category class
+ * @Description Clase que contiene los metodos necesarios para serializar las claves dicotomicas
+ */
+
 public class GeneradorClaves implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -26,6 +33,13 @@ public class GeneradorClaves implements Serializable {
 
 	public TreeMap<String, ArrayList<Object>> claves;
 
+	/**
+	 * @name GeneradorClaves
+	 * @author Adrian Anton Garcia
+	 * @category constructor
+	 * @Description Constructor de generadorClaves, inicializa el mapa de claves a serialozar
+	 */
+	
 	public GeneradorClaves() {
 		claves = new TreeMap<String, ArrayList<Object>>();
 	}
@@ -40,40 +54,51 @@ public class GeneradorClaves implements Serializable {
 	 * @name generarClavesFichero
 	 * @author Adrian Anton Garcia
 	 * @category procedimiento
-	 * @Description Procedimiento que serializa las claves en el fichero claves.dat
+	 * @Description Procedimiento que serializa las claves dicotómicas en el
+	 *              fichero claves.dat
 	 */
 
-	public void generarClavesFichero(){
-		TreeMap<String,String> generosEnlaces=this.leerFichero();
-		for(String genero:generosEnlaces.keySet()){
-		    String enlace=generosEnlaces.get(genero).trim();
-		    
-		    if(enlace.trim().equals("vacio")==false){
+	public void generarClavesFichero() {
+		TreeMap<String, String> generosEnlaces = this.leerFichero();
+		for (String genero : generosEnlaces.keySet()) {
+			String enlace = generosEnlaces.get(genero).trim();
+			// Por cada clave de la que dispongamos enlace
+			if (enlace.trim().equals("vacio") == false) {
+				// Creo la clave
 				ClaveDicotomica clave = new ClaveDicotomica(enlace, nodoInicial);
 				clave.cargarClaveDicotomica(nodoInicial);
 				ArrayList<Object> mapas = clave.devolverMapas();
 				claves.put(genero, mapas);
 				System.out.println(generosEnlaces.get(genero));
-		    }else{
-		    	ArrayList<Object> mapas=new ArrayList<Object>();
-		    	mapas.add(enlace);
-		    	claves.put(genero, mapas);
-		    }
+			} else {
+				ArrayList<Object> mapas = new ArrayList<Object>();
+				mapas.add(enlace);
+				claves.put(genero, mapas);
+			}
 		}
 		try {
-            ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(nombreFichero));
-            ficheroSalida.writeObject(claves);
-            ficheroSalida.flush();
-            ficheroSalida.close();
-            System.out.println("Claves guardadas correctamente...");
- 
-        } catch (FileNotFoundException fnfe) {
-            System.out.println("Error: El fichero no existe. ");
-        } catch (IOException ioe) {
-            System.out.println("Error: Fallo en la escritura en el fichero. ");
-        }
-		
+			// serializo con todas las claves cargadas
+			ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(nombreFichero));
+			ficheroSalida.writeObject(claves);
+			ficheroSalida.flush();
+			ficheroSalida.close();
+			System.out.println("Claves guardadas correctamente...");
+
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("Error: El fichero no existe. ");
+		} catch (IOException ioe) {
+			System.out.println("Error: Fallo en la escritura en el fichero. ");
+		}
+
 	}
+
+	/**
+	 * @name leerFichero
+	 * @author Adrian Anton Garcia
+	 * @category metodo
+	 * @Description Metodo que lee el fichero de los generos con las claves y
+	 *              carga un treemap
+	 */
 
 	public TreeMap<String, String> leerFichero() {
 		ArrayList<String> generosEnlaces = null;

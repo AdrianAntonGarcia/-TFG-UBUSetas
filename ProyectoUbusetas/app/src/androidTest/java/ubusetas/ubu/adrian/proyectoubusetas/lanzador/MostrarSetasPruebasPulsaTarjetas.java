bb888579eb060ccbceb0771c4bad0c9b7507675f
Adrian Anton Garcia <adrtler@gmail.com>
2017-12-19 12:52:08 +0100

    Incorporación de los test de integración de la actividad mostrarSetas.

---
+++ ProyectoUbusetas/app/src/androidTest/java/ubusetas/ubu/adrian/proyectoubusetas/lanzador/MostrarSetasPruebasPulsaTarjetas.java
@@ -0,0 +1,84 @@
+package ubusetas.ubu.adrian.proyectoubusetas.lanzador;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ubusetas.ubu.adrian.proyectoubusetas.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MostrarSetasPruebasPulsaTarjetas {
+
+    @Rule
+    public ActivityTestRule<Lanzadora> mActivityTestRule = new ActivityTestRule<>(Lanzadora.class);
+
+    @Test
+    public void pulsar20tarjetas() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.boton_ir_setas_lanzadora),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.CoordinatorLayout")),
+                                        1),
+                                4),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+        ViewInteraction recyclerView;
+
+        for(int i =0;i<19;++i) {
+            recyclerView = onView(
+                    allOf(withId(R.id.recycler_view_lista_setas),
+                            childAtPosition(
+                                    withClassName(is("android.widget.RelativeLayout")),
+                                    1)));
+            recyclerView.perform(actionOnItemAtPosition(i, click()));
+            pressBack();
+        }
+
+
+
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
